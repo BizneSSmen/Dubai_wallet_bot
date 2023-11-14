1b2f33aac74a618b60c095fae691b9bc9301b64c
ICockBoy <liviolettassfinger@gmail.com>
2023-11-14 21:35:59 +0300

    1

--- Admin_bot/main.py
+++ Admin_bot/main.py
@@ -3,16 +3,12 @@ import logging
 
 from aiogram import Bot, Dispatcher
 
-from config import loadConfig, Config
-from Routers import routers
-
 
 async def main():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     )
-    config = loadConfig("config.env").tgBot
 
     bot: Bot = Bot(token='6564981419:AAGH-tz6Edur9AOlcHAG8gPvi5iDigY4hHU', parse_mode="HTML")
 
